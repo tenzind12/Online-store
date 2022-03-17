e607e140af14bb826e279e68b175c66475ba15ee
Tenzin Dekyong <dhontenzin@gmail.com>
2022-03-17 21:27:13 +0100

    working on login

--- src/Login.jsx
+++ src/Login.jsx
@@ -3,11 +3,44 @@ import React, { useEffect, useState } from 'react';
 let Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [dirty, setDirty] = useState({ email: false, password: false });
+  const [errors, setErrors] = useState({ email: [], password: [] });
 
   // changing document title
   useEffect(() => {
     document.title = 'Login - eCommerce';
   }, []);
+
+  const validate = () => {
+    const errorData = {};
+
+    // E M A I L  V A L I D A T I O N
+    errorData.email = [];
+    if (!email) {
+      errorData.email.push('Email cannot be blank');
+    }
+    const emailRegex = /\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*0;/;
+    if (email) {
+      if (!emailRegex.test(email)) {
+        errorData.email.push('Email format is not accepted');
+      }
+    }
+
+    // P A S S W O R D  V A L I D A T I O N
+    errorData.password = [];
+    if (!password) {
+      errorData.password.push('Password cannot be blank');
+    }
+    const passwordRegex = /((?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,15})/;
+    if (password) {
+      if (!passwordRegex.test(password)) {
+        errorData.password.push(
+          'Password should be 6 - 15 characters long with at least one uppercase letter, one lowercase letter and one digit'
+        );
+      }
+    }
+    setErrors({ errorData });
+  };
 
   return (
     <div className="row">
@@ -32,6 +65,10 @@ let Login = () => {
                 onChange={(event) => {
                   setEmail(event.target.value);
                 }}
+                onBlur={(e) => {
+                  setDirty({ ...dirty, [e.target.name]: true });
+                  validate();
+                }}
                 placeholder="Email"
               />
             </div>
@@ -49,6 +86,10 @@ let Login = () => {
                 placeholder="Password"
                 onChange={(event) => {
                   setPassword(event.target.value);
+                }}
+                onBlur={(e) => {
+                  setDirty({ ...dirty, [e.target.name]: true });
+                  validate();
                 }}
               />
             </div>
